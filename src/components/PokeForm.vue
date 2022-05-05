ec3c9342e4450c98dc953c83e5daa44bbc7ad8bd
Demian <learc47@gmail.com>
2022-05-05 12:20:32 -0400

    init repo

---
+++ src/components/PokeForm.vue
@@ -0,0 +1,79 @@
+<template>
+<div>
+   <img src="../assets/wallpaper.jpg" alt="" class="img-fluid">
+    <h1 class="font-weight-bold m-5">{{ title }}</h1>
+    <label >Nombre:  </label>
+    <input v-model="nombre">
+    <button @click="buscandoPokemon">Buscar</button>
+    <div>
+    <h3 class="card-title font-weight-bold my-4">{{personajes.name}}</h3>
+    <img :src="imagen">
+    <div class="card container my-5 p-0" style="width: 50%;">
+      <div class="card-body">
+        <h5 class="card-title font-weight-bold text-dark">Movimientos</h5>
+        <p v-for="(movimiento, index) in movimientos" :key="index" class="card-text">{{movimiento.move.name}}</p>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title font-weight-bold text-dark">Habilidades</h5>
+        <p v-for="(habilidad, index) in habilidades" :key="index" class="card-text">{{habilidad.ability.name}}</p>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "PokeForm",
+  props: {
+    title: String
+  },
+  data: () => ({
+     nombre:"",
+     personajes: null,
+  }),
+  methods:{
+    async buscandoPokemon(){
+      if(this.nombre !== ""){
+         this.getApiPokemon(this.nombre)
+      }
+    },
+    async getApiPokemon(nombre){
+      try{
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
+        let data = await response.json();
+        this.personajes = data;
+
+      } catch (error) {
+        alert('Nombre invalido')
+        console.log(error)}
+
+    },
+  },
+  computed: {
+    imagen(){
+      return (
+        (this.personajes && this.personajes.sprites && this.personajes.sprites.front_default) || "");
+    },
+    habilidades(){
+      return (this.personajes && this.personajes.abilities) || [];
+    },
+    movimientos(){
+      return (this.personajes && this.personajes.moves) || [];
+    }
+  },
+  created() {
+    this.nombre= "pikachu";
+    this.buscandoPokemon();
+  }
+
+}
+</script>
+
+<style>
+
+ul{
+  list-style: none;
+  margin: 0 auto;
+}
+</style>
